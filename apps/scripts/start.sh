e0c92bc09a0e2e76fa63819c07f0e6b4f78cf867
nathanael rayapin <nathanael.rayapin@gmail.com>
2025-12-18 22:14:26 +0100

    refactor: Script.sh and compose yml moved and refactored

---
+++ apps/scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "🔐 Starting Project in dev mode..."
+
+if ! docker compose up -d angular; then
+  echo "❌ Failed to start Project"
+  exit 1
+fi
+
+echo "✅ Project is up and running! Visit http://localhost:8080 for Keycloak and http://localhost:4200 for Angular"

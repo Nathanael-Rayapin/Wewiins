9fb8c25f32fc37d831d293fc77685cb38af96813
nathanael rayapin <nathanael.rayapin@gmail.com>
2025-12-31 11:59:20 +0100

    fix: Angular initialized withotu ssr

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Keycloak from 'keycloak-js';
+
+export let keycloak: Keycloak;
+
+function initializeKeycloak(): () => Promise<boolean> {
+  return () => {
+    keycloak = new Keycloak({
+      url: 'http://localhost:8080',
+      realm: 'wewiins',
+      clientId: 'saas'
+    });
+
+    return keycloak.init({
+      onLoad: 'login-required',
+    });
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAppInitializer(initializeKeycloak()),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes)
   ]
 };

72aa93b366ca7f18e6db98d6fbe2db3d7e6fd137
Nathanael <nathanael.rayapin@gmail.com>
2026-01-12 23:43:39 +0100

    feat: Controller added for managed activity revenue

---
+++ apps/api/src/main/kotlin/com/wewiins/saas_api/controllers/ActivityController.kt
@@ -0,0 +1,25 @@
+package com.wewiins.saas_api.controllers
+
+import com.wewiins.saas_api.models.ActivityRevenue
+import com.wewiins.saas_api.services.ActivityService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/activity")
+class ActivityController(
+    private val activityService: ActivityService
+) {
+
+    @GetMapping("/revenue/{connectedAccountId}")
+    fun getRevenueByProvider(
+        @PathVariable connectedAccountId: String,
+        @RequestParam startDate: Long,
+        @RequestParam endDate: Long
+    ): List<ActivityRevenue> {
+        return activityService.getPaymentIntentsByQuery(connectedAccountId, startDate, endDate)
+    }
+}

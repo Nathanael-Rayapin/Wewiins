c7416ededafe5b2e5cb3963e2735951fc1363749
Nathanael <nathanael.rayapin@gmail.com>
2026-01-19 00:00:59 +0100

    feat: Translate date range from english to french

--- apps/client/src/app/interfaces/date.ts
+++ apps/client/src/app/interfaces/date.ts
@@ -1,4 +1,12 @@
+// For API processing 
 export type IDateFrom = "aWeekAgo" | "aMonthAgo" | "aYearAgo";
+
+// For UI display
+export const dateRangeOptions: Record<IDateFrom, string> = {
+    aWeekAgo: 'Semaine',
+    aMonthAgo: 'Mois',
+    aYearAgo: 'Année'
+  };
 
 export interface IDateRange {
     startDate: number;

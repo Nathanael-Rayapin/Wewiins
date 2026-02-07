cfc6c34e30862f794bf41d457f18816ec663fd47
Nathanael <nathanael.rayapin@gmail.com>
2026-02-07 16:19:44 +0100

    feat: Dashboard card added

---
+++ apps/client/src/app/components/dashboard-card/dashboard-card.ts
@@ -0,0 +1,42 @@
+import { Component, computed, effect, input, signal } from '@angular/core';
+import { IStatComparison } from '../../dto/dashboard';
+import { DashboardStatKey } from '../../pages/dashboard/data/dashboard.data';
+import { NgOptimizedImage } from '@angular/common';
+import { getRangeAndFormat } from '../../utils/date';
+
+@Component({
+  selector: 'app-dashboard-card',
+  imports: [NgOptimizedImage],
+  templateUrl: './dashboard-card.html',
+  styleUrl: './dashboard-card.css',
+})
+export class DashboardCard {
+  label = input.required<string>();
+  stat = input.required<IStatComparison<number>>();
+  iconPath = input.required<string>();
+  key = input.required<DashboardStatKey>();
+
+  filterRangeDays = input.required<number>();
+  filterRangeDaysStr = signal<string>('');
+
+  constructor() {
+    effect(() => {
+      if (this.stat() && this.filterRangeDays()) {
+        this.filterRangeDaysStr.set(getRangeAndFormat(this.stat(), this.filterRangeDays()));
+      }
+    })
+  }
+
+  protected displayValue = computed(() => {
+    const value = this.stat().currentValue;
+
+    switch (this.key()) {
+      case 'totalRevenue':
+        return `${value.toLocaleString('fr-FR', { maximumFractionDigits: 2 })}`;
+      case 'averageScore':
+        return `${value}/5`;
+      default:
+        return value.toString();
+    }
+  });
+}

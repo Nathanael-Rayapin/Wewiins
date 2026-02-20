bf6ee74bbd1a683379a8b0733c284759e035d400
Nathanael <nathanael.rayapin@gmail.com>
2026-02-20 15:26:53 +0100

    feat: :zap: Title section improved

--- apps/client/src/app/components/title-section/title-section.ts
+++ apps/client/src/app/components/title-section/title-section.ts
@@ -1,16 +1,37 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, input, signal } from '@angular/core';
 import { Button } from '../button/button';
-import { KeycloakService } from '../../services/keycloak.service';
-import { NgOptimizedImage } from '@angular/common';
+import { BurgerMenu } from '../burger-menu/burger-menu';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { IconSvg } from '../icon-svg/icon-svg';
 
 @Component({
   selector: 'app-title-section',
-  imports: [Button, NgOptimizedImage],
+  imports: [Button, BurgerMenu, IconSvg],
   templateUrl: './title-section.html',
   styleUrl: './title-section.css',
 })
 export class TitleSection {
-  protected readonly keycloakService = inject(KeycloakService);
+  private breakpointObserver = inject(BreakpointObserver);
+  title = input.required<string>();
 
-  isHovered = signal(false);
+  isNotificationHovered = signal(false);
+  isMenuHovered = signal(false);
+  isMenuDisplayed = signal(false);
+
+  isTablet$ = toSignal(
+    this.breakpointObserver
+      .observe(['(min-width: 640px)'])
+      .pipe(map(result => result.matches)),
+    { initialValue: false }
+  );
+
+  constructor() {
+    effect(() => {
+      if (this.isTablet$() && this.isMenuDisplayed()) {
+        this.isMenuDisplayed.set(false);
+      }
+    });
+  }
 }
